c89f12c5a04ea983e5e1ac63d69d61e90a4212b4
__hidehalo <tianchen_cc@yeah.net>
2021-05-27 15:15:26 +0800

    fix service provider error when laravel version is 6.x

--- src/ShrinkCompilerServiceProvider.php
+++ src/ShrinkCompilerServiceProvider.php
@@ -4,7 +4,6 @@
  */
 namespace Hidehalo\View\Compilers;
 
-use Illuminate\View\DynamicComponent;
 use Illuminate\Support\ServiceProvider;
 
 class ShrinkCompilerServiceProvider extends ServiceProvider
@@ -18,7 +17,9 @@ class ShrinkCompilerServiceProvider extends ServiceProvider
     {
         $this->app->singleton('blade.compiler', function () {
             return tap(new ShrinkCompiler($this->app['files'], $this->app['config']['view.compiled']), function ($blade) {
-                $blade->component('dynamic-component', DynamicComponent::class);
+                if (class_exists('Illuminate\View\DynamicComponent')) {
+                    $blade->component('dynamic-component', \Illuminate\View\DynamicComponent::class);
+                }
             });
         });
     }
--- tests/ShrinkCompilerTest.php
+++ tests/ShrinkCompilerTest.php
@@ -5,9 +5,12 @@
 namespace Hidehalo\View\Compilers\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Illuminate\Config\Repository;
 use League\Flysystem\Adapter\Local;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Foundation\Application;
 use Hidehalo\View\Compilers\ShrinkCompiler;
+use Hidehalo\View\Compilers\ShrinkCompilerServiceProvider;
 
 class ShrinkCompilerTest extends TestCase
 {
@@ -32,5 +35,23 @@ class ShrinkCompilerTest extends TestCase
         $this->assertEquals($shrinked, '<html><head><title>Test title</title><body><p>
                         Hello world!
                     </p></body></head></html>');
+    }
+
+    public function testConstructFromContainer()
+    {
+        $app = new Application();
+        $app->register(new ShrinkCompilerServiceProvider($app));
+        $local = new Local(__DIR__);
+        $filesystem = new Filesystem($local);
+        $app['files'] = $filesystem;
+        $cachePath = __DIR__.'/cache';
+        $app['config'] = new Repository([
+            'view' => [
+                'compiled' => $cachePath,
+            ],
+        ]);
+        $compiler = $app->get('blade.compiler');
+        $this->assertInstanceOf(ShrinkCompiler::class, $compiler);
+        $this->assertSame($compiler, $app->get('blade.compiler'));
     }
 }
